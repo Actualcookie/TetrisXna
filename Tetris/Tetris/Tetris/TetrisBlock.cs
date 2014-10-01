3170247e7ce9a21b02f6e3ddeb765ba8660f59e2
Actualcookie <actualcookie@gmail.com>
2014-10-01 16:49:57 +0200

    Methodes en Class TetrisBlock toegevoegd

---
+++ Tetris/Tetris/Tetris/TetrisBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris
+{
+    class TetrisBlock
+    {
+     public bool Collision()
+        {
+            return (true);   
+         /*moet zorgen voor de stilstand van het block
+             * dus misschien iets als 
+             * 
+             * (if(Collision(true)) 
+             * velocity = 0.0f;)
+              */     }
+    public void Rotation()
+     {
+        //Roteren van de array? Of roteren van het figuur binnen de array. Moet dus ook links en Rechts om.
+     // 
+    }
+
+
+    }
+}
